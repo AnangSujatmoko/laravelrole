1d324c326c525ccf943bd3f721a93d33ecafd774
Anang Sujatmoko <anangsoedjatmoko@gmail.com>
2024-08-07 10:17:50 +0700

    first commit

---
+++ resources/views/backend/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- footer area start-->
+<footer>
+    <div class="footer-area">
+        <p>© Copyright 2024. All right reserved. Template by <a href="">Anang</a>.</p>
+    </div>
+</footer>
+<!-- footer area end-->
